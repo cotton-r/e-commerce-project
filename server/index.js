b9fe743c7018b8eb87472420809f3663b41b608f
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-09-15 00:58:44 +0100

    import products router

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const pool = require('./db/index');
 const registerRouter = require('./routes/register');
+const productsRouter = require('./routes/products');
 
 const app = express();
 
@@ -9,34 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+// routes
 app.use('/register', registerRouter);
+app.use('/products', productsRouter);
 
 app.get('/', (req, res) => {
   return res.status(200).send({'message': 'first endpoint is working!'})
 });
-
-const getAllProducts = (req, res) => {
-  pool.query('SELECT * FROM products ORDER BY product_id ASC', (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(200).json(results.rows);
-  })
-};
-
-const getProductsByCategory = (req, res) => {
-  pool.query(`SELECT * FROM products WHERE category_name = ${req.query.category_name}`, (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(200).json(results.rows)
-  })
-};
-
-app
- .route('/products')
- // GET endpoint
- .get(getAllProducts)
 
 // start server
 app.listen(process.env.PORT || 3002, () => {
