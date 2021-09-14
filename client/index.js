e613561f3134ee88ca79337fde6278640aab4d4f
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-09-14 19:40:27 +0100

    set up express server

---
+++ client/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const { pool } = require('./config');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  return res.status(200).send({'message': 'first endpoint is working!'})
+});
+
+const getAllProducts = (req, res) => {
+  pool.query('SELECT * FROM products ORDER BY product_id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows);
+  })
+};
+
+const getProductsByCategory = (req, res) => {
+  pool.query(`SELECT * FROM products WHERE category_name = ${req.query.category_name}`, (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
+};
+
+app
+ .route('/products')
+ // GET endpoint
+ .get(getAllProducts)
+
+// start server
+app.listen(process.env.PORT || 3002, () => {
+  console.log('Server listening')
+});
