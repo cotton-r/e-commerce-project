d46a3eb0a18dee6ea8c5c49b48197305f5fcf7ac
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-09-15 01:30:40 +0100

    create products routes

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,28 +2,24 @@ const express = require('express');
 const pool = require('../db/index');
 const productsRouter = express.Router();
 
-productsRouter.get('/', async (req, res) => {
-
-  const getAllProducts = (req, res) => {
-    pool.query('SELECT * FROM products ORDER BY product_id ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      res.status(200).json(results.rows);
-    })
-  };
-  
-  const getProductsByCategory = (req, res) => {
-    pool.query(`SELECT * FROM products WHERE category_name = ${req.query.category_name}`, (error, results) => {
-      if (error) {
-        throw error
-      }
-      res.status(200).json(results.rows)
-    })
-  };
-
-
+// get all products
+productsRouter.get('/', (req, res) => {
+  pool.query('SELECT * FROM products ORDER BY product_id ASC', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(results.rows);
+  });
 });
+  
+// get products by category
+productsRouter.get('/:category', (req, res) => {
+  pool.query(`SELECT * FROM products WHERE category_name = ${req.query.category_name}`, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(results.rows);
+  });
+})
 
 module.exports = productsRouter;
-
