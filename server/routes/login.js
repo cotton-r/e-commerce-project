b1043da673c86fd6e345cda4b4246e1c3727e837
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-09-17 01:55:35 +0100

    set up login route with passport authentication

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const loginRouter = express.Router();
+const passport = require('passport');
 
+loginRouter.post('/', passport.authenticate('local', {
+  successRedirect: ':/',
+  failureRedirect: ':/login',
+  failureFlash: true
+}), (req, res) => {
 
+})
 
 module.exports = loginRouter;
