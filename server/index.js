a17ad32eb42798a8000b63d0ee6be4143a9aeaf5
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-09-17 01:23:03 +0100

    set up passport and express-session in main server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,36 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express');
 const cors = require('cors');
+const initializePassport = require('./config/passport');
+const flash = require('express-flash');
+const session = require('express-session');
+
 const registerRouter = require('./routes/register');
 const productsRouter = require('./routes/products');
 const loginRouter = require('./routes/login');
 
+const initializePassport = require('./config/passport');
+const passport = require('passport');
+
+initializePassport();
+
 const app = express();
 
+// config
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(flash());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // routes
 app.use('/register', registerRouter);
