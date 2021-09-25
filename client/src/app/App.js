b5890af42ed6557ffbe8c2202b33000ad82ddc49
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-09-25 17:26:30 +0100

    set up initial App component with Routes

---
+++ client/src/app/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+const App = () => {
+    return (
+        <Router>
+            <div className='App'>
+                <Nav />
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/register' exact component={Register} />
+                    <Route path='/login' exact component={Login} />
+                    <Route path='/logout' exact component={Logout} />
+                    <Route path='/products' exact component={Products} />
+                </Switch>
+            </div>
+        </Router>
+    );
+};
+
+export default App;
