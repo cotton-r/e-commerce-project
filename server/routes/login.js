eac8fa878fb5c13110dae0084a0821226dd62093
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-09-28 21:00:51 +0100

    redirect user to home page after succesful login

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,6 +3,7 @@ const loginRouter = express.Router();
 const passport = require('passport');
 
 loginRouter.post('/', passport.authenticate('local', {
+  successRedirect: '/',
   failureRedirect: '/login',
 }), (req, res) => {
   res.status(201).json(req.user);
