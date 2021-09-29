483dd7137c2a2ac0f1c9cc53f61e96a9f216e10b
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-09-29 23:19:58 +0100

    import Link

--- client/src/components/products/products.js
+++ client/src/components/products/products.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { loadAllProducts, selectProducts } from './productsSlice';
 
 const Products = () => {
