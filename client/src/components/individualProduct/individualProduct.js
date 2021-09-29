58d5b75866644a31ddb3569df91f068093360a4e
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-09-29 22:16:53 +0100

    create component for individual products

---
+++ client/src/components/individualProduct/individualProduct.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const IndividualProduct = ({product}) => {
+    return (
+        <div className='page-container'>
+            
+        </div>
+    )
+}
