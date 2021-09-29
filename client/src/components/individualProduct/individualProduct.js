7f87f1a88b238a166e0b602ae241b434c18ded2e
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-09-29 23:10:14 +0100

    complete individualProduct component

--- client/src/components/individualProduct/individualProduct.js
+++ client/src/components/individualProduct/individualProduct.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 const IndividualProduct = ({product}) => {
+    const dispatch = useDispatch();
+
     return (
         <div className='page-container'>
-            
+            <div className='individual-product-container'>
+                <p className='individual-product-title'>{product.product_name}</p>
+                <div className='individual-product-image'>
+                    <img src={product.image} alt={product.product_name} />
+                </div>
+                <p className='individual-product-price'>{product.price}</p>
+                <p className='individual-product-description'>{product.description}</p>
+            </div>
+            <div className='add-to-cart-container'>
+                <button onClick={dispatch(addToCart(product))}>Add to Cart</button>
+            </div>
         </div>
-    )
-}
+    );
+};
+
+export default IndividualProduct;
