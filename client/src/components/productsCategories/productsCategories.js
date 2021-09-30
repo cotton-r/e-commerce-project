cea2f833740ccf7702efccc873f5a8373df4d1c0
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-09-30 19:59:50 +0100

    create categories component

---
+++ client/src/components/productsCategories/productsCategories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+const productsCategories = () => {
+    return (
+        <div>
+            <p>Hello.</p>
+        </div>
+    );
+};
+
+export default productsCategories;
