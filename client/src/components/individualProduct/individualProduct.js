4adf2a2e5b2d84db05922e03a0921dece6b5cd8c
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-01 22:45:33 +0100

    finally fix render issue, had to map contents of array before unpacking

--- client/src/components/individualProduct/individualProduct.js
+++ client/src/components/individualProduct/individualProduct.js
@@ -2,41 +2,32 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from "react-router-dom";
-import { Link } from 'react-router-dom';
 import { loadIndividualProduct, selectIndividualProduct } from './individualProductSlice';
 
 const IndividualProduct = () => {
     const dispatch = useDispatch();
-    const { category, product } = useParams();
+    const { product } = useParams();
     const decodedProduct = decodeURIComponent(product);
     const individualProduct = useSelector(selectIndividualProduct);
-    
+    console.log(individualProduct);
+
     // load the single product from database
     useEffect(() => {
         dispatch(loadIndividualProduct(decodedProduct));
-    }, [dispatch, decodedProduct]);
-
-    // return (
-    //     <div className='page-container'>
-    //         <div className='individual-product-container'>
-    //             <p className='individual-product-title'>{product.product_name}</p>
-    //             <div className='individual-product-image'>
-    //                 <img src={product.image} alt={product.product_name} />
-    //             </div>
-    //             <p className='individual-product-price'>{product.price}</p>
-    //             <p className='individual-product-description'>{product.description}</p>
-    //         </div>
-    //         <div className='add-to-cart-container'>
-    //             {/* <button onClick={dispatch(addToCart(product))}>Add to Cart</button> */}
-    //         </div>
-    //     </div>
-    // );
+    }, [dispatch]);
 
     return (
-        <div>
-            <p>hello</p>
+        <div className='page-container'>
+            {individualProduct.map((product) => (
+                <div>
+                    <p>{product.product_name}</p>
+                    <p>{product.price}</p>
+                    <img src={product.image} alt={product.product_name} height='500px' width='auto'/>
+                    <p>{product.description}</p>
+                </div>
+            ))}
         </div>
-    )
+    );
 };
 
 export default IndividualProduct;
