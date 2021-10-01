372c86474c6966cf6ef8a6a38d7069341a267998
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-01 02:06:21 +0100

    object destructure the route param, and decodeURIComponent to get product name

--- client/src/components/individualProduct/individualProduct.js
+++ client/src/components/individualProduct/individualProduct.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-const IndividualProduct = ({product}) => {
+const IndividualProduct = () => {
     const dispatch = useDispatch();
+    const { product } = useParams();
+    const decodedProduct = decodeURIComponent(product);
+
+    // return (
+    //     <div className='page-container'>
+    //         <div className='individual-product-container'>
+    //             <p className='individual-product-title'>{product.product_name}</p>
+    //             <div className='individual-product-image'>
+    //                 <img src={product.image} alt={product.product_name} />
+    //             </div>
+    //             <p className='individual-product-price'>{product.price}</p>
+    //             <p className='individual-product-description'>{product.description}</p>
+    //         </div>
+    //         <div className='add-to-cart-container'>
+    //             {/* <button onClick={dispatch(addToCart(product))}>Add to Cart</button> */}
+    //         </div>
+    //     </div>
+    // );
 
     return (
-        <div className='page-container'>
-            <div className='individual-product-container'>
-                <p className='individual-product-title'>{product.product_name}</p>
-                <div className='individual-product-image'>
-                    <img src={product.image} alt={product.product_name} />
-                </div>
-                <p className='individual-product-price'>{product.price}</p>
-                <p className='individual-product-description'>{product.description}</p>
-            </div>
-            <div className='add-to-cart-container'>
-                {/* <button onClick={dispatch(addToCart(product))}>Add to Cart</button> */}
-            </div>
+        <div>
+            <p>hello</p>
         </div>
-    );
+    )
 };
 
 export default IndividualProduct;
