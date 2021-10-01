616c5215bc63e85111a3f100657269ab22dd99cc
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-01 02:15:29 +0100

    create slice

---
+++ client/src/components/individualProduct/individualProductSlice.js
@@ -0,0 +1,32 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const loadIndividualProduct = createAsyncThunk('individualProduct/loadIndividualProduct', async (product) => {
+    const response = await fetch(`/products/${product}`);
+    const json = await response.json();
+
+    return json;
+});
+
+export const individualProductSlice = createSlice({
+    name: 'individualProduct',
+    initialState: {
+        individualProduct: [],
+        loadindividualProductStatus: 'idle',
+    },
+    extraReducers: {
+        [loadIndividualProduct.pending]: (state, action) => {
+            state.loadindividualProductStatus = 'loading';
+        },
+        [loadIndividualProduct.fulfilled]: (state, action) => {
+            state.loadindividualProductStatus = 'succeeded';
+            state.individualProduct = action.payload;
+        },
+        [loadIndividualProduct.rejected]: (state, action) => {
+            state.loadindividualProductStatus = 'failed';
+        },
+    }
+});
+
+export const selectIndividualProduct = (state) => state.individualProduct.individualProduct;
+
+export default individualProductSlice.reducer;
