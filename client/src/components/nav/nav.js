40961d8812485e456581ca40cf4c8bc55785e11f
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-04 20:23:29 +0100

    add logout button

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -3,6 +3,12 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Nav = () => {
 
+    const logout = async () => {
+        const response = await fetch('/logout');
+        const json = await response.json();
+        return json;
+    }
+
     return (
         <nav className='nav-bar'>
             <p className='title'>The Football Shop</p>
@@ -10,6 +16,7 @@ const Nav = () => {
                 <Link to='/login'>
                     <p>Login</p>
                 </Link>
+                <button onClick={() => logout()}>Logout</button>
             </div>
         </nav>
     );
