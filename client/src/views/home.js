b46310ece1210ddc49969d193ace41f4f3c9866e
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-05 01:56:21 +0100

    move sessionStorage cookie logic to Login.js

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import Cookies from 'universal-cookie';
+
 
 const Home = () => {
-
-    const cookies = new Cookies();
-    const sessionCookie = cookies.get('currentsession');
-    console.log(sessionCookie);
-
     return (
         <div>
             <p>Home Page</p>
