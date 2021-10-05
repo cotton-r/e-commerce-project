cbb07016ba9d39067a6a7c426c745a24a6da1749
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-05 01:56:56 +0100

    store session cookie in SessionStorage, change form submit to handleSubmit function using fetch()

--- client/src/views/login.js
+++ client/src/views/login.js
@@ -1,18 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 
 const Login = () => {
+
+    const cookies = new Cookies();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = new FormData();
+        data.append('email', email);
+        data.append('password', password);
+        
+        await fetch('/login', {
+            method: 'post',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            }, 
+            body: JSON.stringify({
+                email: email,
+                password: password
+            }),
+        });
+
+        // get cookie sent from passport.serializeUser()
+        const sessionCookie = await cookies.get('currentsession');
+    
+        // if cookie is present (user logged in) then add to sessionStorage so available globally
+        if (sessionCookie) {
+            await sessionStorage.setItem('session', 'active');
+        };
+        window.location.href = '/';
+    };
 
     return (
         <div className='page-container'>
             <h2>Please log in.</h2>
             <div className='form-container'>
-                <form method='POST' action='/login'>
+                <form onSubmit={handleSubmit}>
                     <div>
-                        <input type='email' id='email' name='email' placeholder='Email Address' required />
+                        <input 
+                            type='email' 
+                            id='email' 
+                            name='email' 
+                            value={email} 
+                            placeholder='Email Address' 
+                            required 
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
                     <div>
-                        <input type='password' id='password' name='password' placeholder='Password' required />
+                        <input 
+                            type='password' 
+                            id='password' 
+                            name='password' 
+                            value={password }
+                            placeholder='Password' 
+                            required 
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
                     <p>Don't have an account?</p>
                     <Link to='/register'>
