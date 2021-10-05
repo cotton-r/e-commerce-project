060da3bc2f78cc96db114d187ae5f9238edb9aa8
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-05 22:16:07 +0100

    add styles

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/home.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+
+    const hover = () => {
+        document.getElementById('page').style={boxShadow: 'inset 0 0 0 1000px rgba(0,0,0,.2)'};     
+    }
+
     return (
-        <div className='homepage-container'>
-            <p>Home Page</p>
+        <div className='homepage-container' id='page'>
+            <div className='layer'>
+                <Link to='/products'>
+                    <button className='home-button' onMouseEnter={hover}><span>View Products</span></button>
+                </Link>
+            </div>
         </div>
     );
 };
