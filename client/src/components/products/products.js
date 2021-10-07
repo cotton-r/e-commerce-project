88b76dd35130ce76274fa53a0e1c38b14993ca81
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-07 02:41:49 +0100

    amend classnames and wrapper divs

--- client/src/components/products/products.js
+++ client/src/components/products/products.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { loadAllProducts, selectProducts } from './productsSlice';
 
+import '../../styles/products.css';
+
 const Products = () => {
     const dispatch = useDispatch();
     const allProducts = useSelector(selectProducts);
@@ -14,12 +16,27 @@ const Products = () => {
     }, [dispatch]);
         
     return (
-        <div className='page-container'>
+        <div className='products-page-container'>
             {allProducts.map((product) => (
                 <Link to={`/products/${product.category_name}/${encodeURIComponent(product.product_name)}`}>
-                    <div key={product.product_id}>
-                        <p>{product.product_name}</p>
-                        <p>{product.price}</p>
+                    <div className='product-tile' key={product.product_id}>
+                        <div className='product-tile-wrapper'>
+                            <div className='title-wrapper'>
+                                <p>{product.product_name}</p>
+                            </div>
+                            <div className='image-container'>
+                                <img src={product.image} alt={product.product_name} height='auto' width='300px'/>
+                            </div>
+                            <p>£{product.price}</p>
+                            <button className='add-to-cart-button' onClick={() => {
+                                if (!sessionStorage.getItem(JSON.stringify(product.product_id))) {
+                                    sessionStorage.setItem(JSON.stringify(product.product_id), 1);
+                                } else {
+                                    const i = sessionStorage.getItem(JSON.stringify(product.product_id));
+                                    sessionStorage.setItem(JSON.stringify(product.product_id), Number(i) + 1);
+                                }
+                            }}>Add to cart</button>
+                        </div>
                     </div>
                 </Link>
             ))}
