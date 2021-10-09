bb8a16e000af133e1ff8701b2323b69703c126be
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-10 00:32:51 +0100

    create html structure for individualProduct pages

--- client/src/components/individualProduct/individualProduct.js
+++ client/src/components/individualProduct/individualProduct.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from "react-router-dom";
 import { loadIndividualProduct, selectIndividualProduct } from './individualProductSlice';
+import '../../styles/individualProduct.css';
 
 const IndividualProduct = () => {
     const dispatch = useDispatch();
@@ -15,24 +16,30 @@ const IndividualProduct = () => {
     }, [dispatch, product]);
 
     return (
-        <div className='page-container'>
+        <div className='individual-product-container'>
             {individualProduct.map((product) => (
                 <div> 
-                    <div>
-                        <p>{product.product_name}</p>
-                        <p>{product.price}</p>
-                        <img src={product.image} alt={product.product_name} height='500px' width='auto'/>
-                        <p>{product.description}</p>
-                    </div>
-                    <div>
-                        <button className='add-to-cart-button' onClick={() => {
-                            if (!sessionStorage.getItem(JSON.stringify(product.product_id))) {
-                                sessionStorage.setItem(JSON.stringify(product.product_id), 1);
-                            } else {
-                                const i = sessionStorage.getItem(JSON.stringify(product.product_id));
-                                sessionStorage.setItem(JSON.stringify(product.product_id), Number(i) + 1);
-                            }
-                        }}>Add to cart</button>
+                    <p className='individual-product-name'>{product.product_name}</p>
+                    <div className='individual-product-wrapper'>
+                        <div>    
+                            <img src={product.image} alt={product.product_name} width='560px' height='auto'/>
+                        </div>
+                        <div className='individual-product-details'>
+                            <div className='description-wrapper'>
+                                <p className='description-title'>Product description:</p>
+                                <p className='individual-product-description'>{product.description}</p>
+                            </div>
+                            <p className='individual-product-price'>£{product.price}</p>
+                    
+                            <button className='add-to-cart-button' onClick={() => {
+                                if (!sessionStorage.getItem(JSON.stringify(product.product_id))) {
+                                    sessionStorage.setItem(JSON.stringify(product.product_id), 1);
+                                } else {
+                                    const i = sessionStorage.getItem(JSON.stringify(product.product_id));
+                                    sessionStorage.setItem(JSON.stringify(product.product_id), Number(i) + 1);
+                                }
+                            }}>Add to cart</button>
+                        </div>
                     </div>
                 </div>
             ))}
