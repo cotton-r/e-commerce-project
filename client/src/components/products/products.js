ea7aeb41c9dad62a73e22cb5537fd326c75244c3
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-09 21:18:12 +0100

    add more css classes and change title positioning in document flow

--- client/src/components/products/products.js
+++ client/src/components/products/products.js
@@ -21,21 +21,23 @@ const Products = () => {
                 <Link to={`/products/${product.category_name}/${encodeURIComponent(product.product_name)}`}>
                     <div className='product-tile' key={product.product_id}>
                         <div className='product-tile-wrapper'>
-                            <div className='title-wrapper'>
-                                <p>{product.product_name}</p>
-                            </div>
                             <div className='image-container'>
                                 <img src={product.image} alt={product.product_name} height='auto' width='300px'/>
                             </div>
-                            <p>£{product.price}</p>
-                            <button className='add-to-cart-button' onClick={() => {
-                                if (!sessionStorage.getItem(JSON.stringify(product.product_id))) {
-                                    sessionStorage.setItem(JSON.stringify(product.product_id), 1);
-                                } else {
-                                    const i = sessionStorage.getItem(JSON.stringify(product.product_id));
-                                    sessionStorage.setItem(JSON.stringify(product.product_id), Number(i) + 1);
-                                }
-                            }}>Add to cart</button>
+                            <div className='title-wrapper'>
+                                <p>{product.product_name}</p>
+                            </div>
+                            <p className='product-tile-price'>£{product.price}</p>
+                            <div className='product-tile-button-wrapper'>
+                                <button className='add-to-cart-button' onClick={() => {
+                                    if (!sessionStorage.getItem(JSON.stringify(product.product_id))) {
+                                        sessionStorage.setItem(JSON.stringify(product.product_id), 1);
+                                    } else {
+                                        const i = sessionStorage.getItem(JSON.stringify(product.product_id));
+                                        sessionStorage.setItem(JSON.stringify(product.product_id), Number(i) + 1);
+                                    }
+                                }}>Add to cart</button>
+                            </div>
                         </div>
                     </div>
                 </Link>
