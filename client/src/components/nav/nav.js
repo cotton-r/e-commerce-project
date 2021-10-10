96dd8b52ef9ae9895c2f78a72e4a7ee1ca90cd9e
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-10 03:45:10 +0100

    add nav links to navbar

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -40,6 +40,18 @@ const Nav = ({isLoggedIn}) => {
             <Link to='/'>
                 <h1 className='nav-title'>The Football Shop</h1>
             </Link>
+            <div className='nav-menu'>
+                <div className='dropdown'>
+                    <button className='dropbtn'><span>Products</span></button>
+                    <div className='dropdown-content'>
+                        <Link to='/products/boots'><a>Boots</a></Link>
+                        <Link to='/products/footballs'><a>Footballs</a></Link>
+                        <Link to='/products/kits'><a>Kits</a></Link>
+                    </div>
+                </div>
+                <Link to='/'><a><span>About us</span></a></Link>
+                <Link to='/cart'><a><span>Cart</span></a></Link>
+            </div>
             <div className='nav-links'>
                 {isUserLoggedIn()}                
             </div>
