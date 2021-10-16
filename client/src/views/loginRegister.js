ef6d50ecb036f8e552ee20ccca2f5b1c23c3a7a1
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-16 20:12:01 +0100

    create dual LoginRegister page

--- client/src/views/loginRegister.js
+++ client/src/views/loginRegister.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 
+import '../styles/loginRegister.css';
+
 const LoginRegister = () => {
+
+    const [isSignUp, setSignUp] = useState(true);
 
     const cookies = new Cookies();
 
@@ -38,82 +42,92 @@ const LoginRegister = () => {
     };
 
     return (
-        <div className='container' id='container'>
-            <div className='form-container sign-up-container'>
-                <h2>Sign up for an account.</h2>
-                <div className='form-container'>
-                    <form method='POST' action='/register'>
-                        <div>
-                            <input type='text' id='name' name='name' placeholder='Name' required />
-                        </div>
-                        <div>
-                            <input type='email' id='email' name='email' placeholder='Email Address' required />
-                        </div>
-                        <div>
-                            <input type='password' id='password' name='password' placeholder='Password' required />
-                        </div>
-                        <div>
-                            <input type='password' id='password2' name='password2' placeholder='Confirm password' required />
-                        </div>
-                        <div>
-                            <button className='form-button' type='submit'>
-                                Register
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div className='form-container sign-in-container'>
-                <h2>Please log in.</h2>
-                <div className='form-container'>
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <input 
-                                type='email' 
-                                id='email' 
-                                name='email' 
-                                value={email} 
-                                placeholder='Email Address' 
-                                required 
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <input 
-                                type='password' 
-                                id='password' 
-                                name='password' 
-                                value={password }
-                                placeholder='Password' 
-                                required 
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </div>
-                        <p>Don't have an account?</p>
-                        <Link to='/register'>
-                            <p>Sign up</p>
-                        </Link>
-                        <div>
-                            <button className='form-button' type='submit'>
-                                Login
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div className='overlay-container'>
-                <div class="overlay">
-                    <div class="overlay-panel overlay-left">
-                        <h1>Welcome Back!</h1>
-                        <p>
-                            To use your existing details please login
-                        </p>
-                        <button class="ghost" id="signIn">Sign In</button>
+        <div className='login-main-wrapper'>
+            <div className={isSignUp ? 'container right-panel-active' : 'container'} id='container'>
+                <div className='form-container sign-up-container'>
+                    <div className='form-container'>
+                        <form method='POST' action='/register'>
+                            <h2>Register</h2>
+                            <div>
+                                <input type='text' id='name' name='name' placeholder='Name' required />
+                            </div>
+                            <div>
+                                <input type='email' id='email' name='email' placeholder='Email Address' required />
+                            </div>
+                            <div>
+                                <input type='password' id='password' name='password' placeholder='Password' required />
+                            </div>
+                            <div>
+                                <input type='password' id='password2' name='password2' placeholder='Confirm password' required />
+                            </div>
+                            <div>
+                                <button className='form-button' type='submit'>
+                                    Register
+                                </button>
+                            </div>
+                        </form>
                     </div>
-                    <div class="overlay-panel overlay-right">
-                        <h1>Hello, Friend!</h1>
-                        <p>Enter your personal details to register</p>
-                        <button class="ghost" id="signUp">Sign Up</button>
+                </div>
+                <div className='form-container sign-in-container'>
+                    <div className='form-container'>
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <h2>Sign in</h2>
+                                <input 
+                                    type='email' 
+                                    id='email' 
+                                    name='email' 
+                                    value={email} 
+                                    placeholder='Email Address' 
+                                    required 
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <input 
+                                    type='password' 
+                                    id='password' 
+                                    name='password' 
+                                    value={password }
+                                    placeholder='Password' 
+                                    required 
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <button className='form-button' type='submit'>
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className='overlay-container'>
+                    <div className="overlay">
+                        <div className="overlay-panel overlay-left">
+                            <h1>Hello, Friend!</h1>
+                            <p>Enter your personal details to register
+                                <br/><br/>
+                                Already have an account? Sign in below
+                            </p>
+                            <button 
+                                className="ghost" 
+                                id="signIn"
+                                onClick={() => setSignUp(false)}
+                            >Sign In</button>
+                        </div>
+                        <div className="overlay-panel overlay-right">
+                            <h1>Welcome Back!</h1>
+                            <p>To use your existing details please login
+                                <br/><br/>
+                                Don't have an account? Sign up below!
+                            </p>
+                            <button 
+                                className="ghost" 
+                                id="signUp"
+                                onClick={() => setSignUp(true)}
+                            >Sign Up</button>
+                        </div>
                     </div>
                 </div>
             </div>
