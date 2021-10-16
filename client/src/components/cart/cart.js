c29db88fe69dd9854f14bc11182bd69cbd10dbb9
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-16 14:03:04 +0100

    create mobile version of cart and make it responsive down to 320xp

--- client/src/components/cart/cart.js
+++ client/src/components/cart/cart.js
@@ -86,6 +86,43 @@ const Cart = ({changeValue}) => {
                     <button className='checkout-btn'>Checkout</button>
                 </div>
             </div>
+
+            {/* mobile version */}
+            <div className='mobile-header'>
+                <div className='price-row'>
+                    <p>Subtotal:</p>
+                    <p>£{Number(totalPrice).toFixed(2)}</p>
+                </div>
+                <div className='price-row'>
+                    <p>Delivery charge:</p>
+                    <p>£0.00</p>
+                </div>
+                <div className='price-row final-price'>
+                    <p>Total:</p>
+                    <p>£{Number(totalPrice).toFixed(2)}</p>
+                </div>
+            </div>
+            <div className='cart-items-section-mobile'>
+                {newItems.map(nested => nested.map(product => {
+                    return <CartItem 
+                                product={product} 
+                                quantity={getQuantity(product.product_id)} 
+                                key={product.product_id}
+                                increaseQty={increaseQty}
+                                decreaseQty={decreaseQty}
+                                removeItem={removeItem}
+                            />
+                }))}
+            </div>
+            <div className='mobile-footer'>
+                <div className='mobile-final-price'>
+                    <p>Total:</p>
+                    <p>£{Number(totalPrice).toFixed(2)}</p>
+                </div>
+                <div className='checkout-btn-wrapper'>
+                    <button className='checkout-btn'>Checkout</button>
+                </div>
+            </div>
         </div>
     )
 
