ba934e1ac831742990d4a619a7d6c27581ff4d60
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-18 21:43:05 +0100

    update css for mobile browsers

--- index.js
+++ index.js
@@ -60,5 +60,5 @@ app.get("*", (req, res) => {
 
 // start server
 app.listen(process.env.PORT || 80, () => {
-  console.log('Server listening')
+  console.log(`Server listening on port ${process.env.PORT || 80}`)
 });
